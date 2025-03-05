b5cb6b93c980a92c9d3717181217136c7f16920a
NourElDin Nabil <nournabil1012@gmail.com>
2025-03-06 01:17:00 +0200

    remove course and trainee nav from login and register pages

--- ITI_Website/ITI_Website/views.py
+++ ITI_Website/ITI_Website/views.py
@@ -30,31 +30,9 @@ def login(request):
 
 
 def register(request):
-    if request.method == "POST":
-        username = request.POST["username"]
-        password = request.POST["password"]
-        confirm_password = request.POST["confirm_password"]
-
-        if password == confirm_password:
-            if User.objects.filter(username=username).exists():
-                messages.error(request, "Username already exists")
-            else:
-                user = User.objects.create_user(username=username, password=password)
-                user.save()
-                messages.success(
-                    request, "Account created successfully. Please log in."
-                )
-                return redirect("login")
-        else:
-            messages.error(request, "Passwords do not match")
-
     return render(request, "register.html")
 
 
 def logout(request):
     auth_logout(request)
     return redirect("home")
-
-
-# def register(request):
-#     return render(request, "register.html")
