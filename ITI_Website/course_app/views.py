6c36db0a72b8ce82cf6d85e70bad32d72b0ca507
NourElDin Nabil <nournabil1012@gmail.com>
2025-03-06 01:30:25 +0200

    update files to get list for views.py

--- ITI_Website/course_app/views.py
+++ ITI_Website/course_app/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def course(request):
-    return render(request,'course.html')
+    courses = [
+        {
+            "id": 0,
+            "name": "HTML",
+            "level": "beginner",
+            "start_date": "2023-01-01",
+            "end_date": "2023-01-15",
+        },
+        {
+            "id": 1,
+            "name": "CSS",
+            "level": "beginner",
+            "start_date": "2023-01-16",
+            "end_date": "2023-01-31",
+        },
+        {
+            "id": 2,
+            "name": "JavaScript",
+            "level": "beginner",
+            "start_date": "2023-02-01",
+            "end_date": "2023-02-15",
+        },
+    ]
+
+    return render(request, "course.html", {"courses": courses})
--- ITI_Website/trainee_app/views.py
+++ ITI_Website/trainee_app/views.py
@@ -1,5 +1,30 @@
 from django.shortcuts import render
 
+
 # Create your views here.
 def trainee(request):
-    return render(request,'trainee.html')
+    trainees = [
+        {
+            "id": 0,
+            "name": "Ahmed Hassan",
+            "age": 23,
+            "level": "beginner",
+            "join_date": "2023-01-04",
+        },
+        {
+            "id": 1,
+            "name": "Ahmed Ali",
+            "age": 20,
+            "level": "beginner",
+            "join_date": "2023-03-06",
+        },
+        {
+            "id": 2,
+            "name": "Mohamed Gamal",
+            "age": 25,
+            "level": "beginner",
+            "join_date": "2023-02-02",
+        },
+    ]
+
+    return render(request, "trainee.html", {"trainees": trainees})
