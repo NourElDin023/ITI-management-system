7df5bdbc187e1c7804dc7a06f2c741c8ea3f16c2
NourElDin Nabil <nournabil1012@gmail.com>
2025-03-11 02:29:51 +0200

    Add PostgreSQL config and models

--- ITI_Website/trainee_app/models.py
+++ ITI_Website/trainee_app/models.py
@@ -1,3 +1,9 @@
 from django.db import models
 
 # Create your models here.
+class Trainee(models.Model):
+    id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+    age = models.IntegerField()
+    level = models.CharField(max_length=100)
+    join_date = models.DateField()
